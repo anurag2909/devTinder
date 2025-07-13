cfea15c4b57fdf8253b422de5320257c4d103bdf
Anurag Pandey <anurag2992000@gmail.com>
2025-07-13 17:32:43 +0530

    Exploring routing methods

--- src/app.js
+++ src/app.js
@@ -2,23 +2,13 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-    res.send("Hello from the server!");
+app.use('/user', (req, res) => {
+    res.send("Hi, I am an user!");
 });
 
-app.use("/getData", (req, res) => {
-    res.send("I am getting the data from it");
+app.post('/users', (req, res) => {
+    res.send("Hi, I am an user to be posted!");
 });
-
-app.use("/test", (req, res) => {
-    res.send("Hello from the server!");
-});
-
-app.use("/test", (req, res) => {
-    res.send("Hello from the server!");
-});
-
-
 
 app.listen(3000, () => {
     console.log("Sever is successfully listening on port 3000");
