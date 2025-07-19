02c18409171c86a9c8c402fbde0a430cc2961ed7
Anurag Pandey <anurag2992000@gmail.com>
2025-07-19 13:01:59 +0530

    Added userSchema and Model

--- src/app.js
+++ src/app.js
@@ -1,18 +1,15 @@
 const express = require("express");
 
 const app = express();
-const { checkAuth } = require("./middlewares/auth");
+const { connectDB } = require("./config/database");
 
-app.use("/admin", checkAuth);
-
-app.use("/admin/getAllData", (req, res) => {
-  res.send("All data sent");
-});
-
-app.use("/admin/deleteAllData", (req, res) => {
-  res.send("All data deleted");
-});
-
-app.listen(3000, () => {
-  console.log("Sever is successfully listening on port 3000");
-});
+connectDB()
+  .then(() => {
+    console.log("Database connection established!");
+    app.listen(7777, () => {
+      console.log("Server is listeming on port no 7777");
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected!");
+  });
---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  mongoose.connect(
+    "mongodb+srv://anurag2992000:xo2YkT8yaNVZNXc6@nodejslearning.j2jhrz7.mongodb.net/"
+  );
+};
+
+module.exports = {
+  connectDB,
+};
