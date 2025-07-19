7c418185ffd7c4ac3e2463f77bd0b22e1bf78f41
Anurag Pandey <anurag2992000@gmail.com>
2025-07-19 14:06:17 +0530

    Init Databse

--- src/app.js
+++ src/app.js
@@ -2,12 +2,29 @@ const express = require("express");
 
 const app = express();
 const { connectDB } = require("./config/database");
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Renu",
+    lastName: "Pandey",
+    emailId: "Renu3112@gmail.com",
+    password: "Renu@123",
+  });
+
+  try {
+    await user.save();
+    res.send("User Added Successfully!");
+  } catch (err) {
+    res.status(400).send("Something saving user " + err.message);
+  }
+});
 
 connectDB()
   .then(() => {
     console.log("Database connection established!");
     app.listen(7777, () => {
-      console.log("Server is listeming on port no 7777");
+      console.log("Server is listening on port no 7777");
     });
   })
   .catch((err) => {
