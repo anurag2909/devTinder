84e91bab84166c61415d49b720b5074560f54458
Anurag Pandey <anurag2992000@gmail.com>
2025-07-24 12:35:32 +0530

    Added Login and Signup API

--- src/app.js
+++ src/app.js
@@ -3,18 +3,54 @@ const express = require("express");
 const app = express();
 const { connectDB } = require("./config/database");
 const User = require("./models/user");
+const { validateSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  //console.log(req.body);
-  const user = new User(req.body);
-
   try {
+    // Validate of the incoming data
+    validateSignUpData(req);
+
+    // Encrypt the password
+    const { firstName, lastName, emailId, password } = req.body;
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    // Creating a new instance of the user model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+
     await user.save();
     res.send("User Added Successfully!");
   } catch (err) {
-    res.status(400).send("Something saving user " + err.message);
+    res.status(400).send("ERROR " + err.message);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    const user = await User.findOne({ emailId: emailId });
+
+    if (!user) {
+      throw new Error("EmailId does not exist");
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new Error("Entered password is incorrect!");
+    } else {
+      res.send("User logged in successfully!");
+    }
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
   }
 });
 
@@ -64,17 +100,17 @@ app.patch("/user", async (req, res) => {
       "skills",
     ];
 
-    const isUpdateAllowed = Object.keys(data).every((k) => 
+    const isUpdateAllowed = Object.keys(data).every((k) =>
       ALLOWED_UPDATES.includes(k)
     );
 
     console.log("isUpdateAllowed -> " + isUpdateAllowed);
 
-    if (!isUpdateAllowed){
+    if (!isUpdateAllowed) {
       throw new Error("Update not allowed!");
     }
 
-    if (data?.skills.length > 10){
+    if (data?.skills.length > 10) {
       throw new Error("Skills cannot be more than 10");
     }
 
