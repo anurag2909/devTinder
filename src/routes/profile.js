44f15caa8bad2c2100432d8703d2052d2cd048e3
Anurag Pandey <anurag2992000@gmail.com>
2025-07-25 08:45:20 +0530

    Logout + edit profile API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,11 +2,32 @@ const express = require("express");
 const profileRouter = express.Router();
 
 const { userAuth } = require("../middlewares/auth");
+const { validateProfileEditData } = require("../utils/validation");
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
     res.send(user);
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateProfileEditData(req)) {
+      throw new Error("Invalid edit request!");
+    }
+
+    const loggedinUser = req.user;
+    console.log(loggedinUser);
+
+    Object.keys(req.body).every((key) => (loggedinUser[key] = req.body[key]));
+    console.log("After editing -> " + loggedinUser);
+
+    await loggedinUser.save();
+    res.send(loggedinUser.firstName + ", your profile was edited successfully!");
+
   } catch (err) {
     res.status(400).send("ERROR: " + err.message);
   }
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -12,6 +12,23 @@ const validateSignUpData = (req) => {
   }
 };
 
+const validateProfileEditData = (req) => {
+  const allowedEditFields = [
+    "firstName",
+    "lastName",
+    "gender",
+    "age",
+    "about",
+    "skills",
+  ];
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowedEditFields.includes(field)
+  );
+
+  return isEditAllowed;
+};
+
 module.exports = {
   validateSignUpData,
+  validateProfileEditData,
 };
