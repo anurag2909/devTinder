546db253b688945f06e77ed456207c78646cbd17
Anurag Pandey <anurag2992000@gmail.com>
2025-07-30 08:24:07 +0530

    A new API to get all the connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,8 @@ const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const userRouter = express.Router();
 
+const USER_SAFE_DATA = "firstName lastName age gender about skills";
+
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
@@ -15,6 +17,34 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     res.json({
       message: "Data fetched successfully!",
       data: connectionRequests,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString())
+        return row.toUserId;
+      else return row.fromUserId;
+    });
+
+    res.json({
+      message: "Data fetched successfully!",
+      data: data,
     });
   } catch (err) {
     res.status(400).send("ERROR: " + err.message);
